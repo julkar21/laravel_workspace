09c317dd912069319b647800a173cec8e312b4ad
unknown <mdjulkarnaen7@gmail.com>
2022-01-21 18:47:08 +0600

    initail-commit

---
+++ Laracommerce/resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello Baby!! How do you do!!!</title>
+</head>
+<body>
+    <h1>Fucking Fucker Fucking Guy After Fucking Telling Lie</h1>
+</body>
+</html>
---
+++ Laracommerce/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[PagesController::class, 'index']);
+
+/*Route::get('/', function () {
+    return view('home');
+});
+*/
+//Route::get('/products', [ProductsController::class, 'index']);
+
+//Route::get('/products/{id}', [ProductsController::class, 'show']);
+
+Route::get('/users', function(){
+    return 6+9;
+});
