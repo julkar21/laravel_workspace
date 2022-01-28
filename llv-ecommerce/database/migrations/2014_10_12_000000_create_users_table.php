c1a121a0975515e13df1e5085d28a62a2b4494d4
unknown <mdjulkarnaen7@gmail.com>
2022-01-28 09:52:23 +0600

    llv-ecommerce-setup

---
+++ llv-ecommerce/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increment('id');
+            $table->string('name',128);
+            $table->string('email',128)->unique();
+            $table->string('phone',32)->unique();
+            $table->string('password',128);
+            $table->tinyInteger('email_verified_at')->nullable(0);
+            $table->string('email_verification_token',80)->nullable;
+            $table->string('facebook_id',32)->nullable;
+            $table->string('google_id',32)->nullable;
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
