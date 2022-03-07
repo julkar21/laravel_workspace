56519f53689eddbdeb5b5a6656de1cd1dde10562
julkar21 <mdjulkarnaen7@gmail.com>
2022-03-07 19:14:13 +0600

    product and Category

--- llv-ecommerce/resources/views/layouts/base.blade.php
+++ llv-ecommerce/resources/views/layouts/base.blade.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Home</title>
+    <base href="/public">
     <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic,900,900italic&amp;subset=latin,latin-ext" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Open%20Sans:300,400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext" rel="stylesheet">
